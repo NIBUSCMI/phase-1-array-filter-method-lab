1dffae5eafd38db7fe84cf68fae0ecb3e8251661
Nick <nbuscem1@binghamton.edu>
2022-06-02 21:41:07 -0400

    commit tree aaa70a1a6e5884dde6f7a65824a309ddd0e77b58

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string) {
+    let result = array.filter(possibleMatch => possibleMatch.toLowerCase() === string.toLowerCase());
+    return result
+}
+//console.log(findMatching(drivers, 'bobby'));
+
+function fuzzyMatch(arr, str) {
+    let res = arr.filter(possMatch => possMatch.charAt(0).toLowerCase() === str.charAt(0).toLowerCase());
+    return res
+}
+console.log(fuzzyMatch(drivers, 'b'))
+
+function matchName(ar, st) {
+    let result = ar.filter(obj => obj.name === st);
+    return result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//array.filter(function(currentValue, index, arr), thisValue)
